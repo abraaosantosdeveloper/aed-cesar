70dc231d59c2c38da7a5f4ff75f5c40a22b5fdb9
abraaosantosdeveloper <abraaofilipi12@gmail.com>
2026-08-25 21:43:15 -0300

    Adicionado nó e métodos de empilhamento e desempilhamento

--- listas/encadeamento-simples/pilhas.cpp
+++ listas/encadeamento-simples/pilhas.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+typedef struct Node {
+    char data;
+    struct Node* nextValue;
+}Node;
+
+Node* stack(Node* top, char value){
+    Node* newValue = new Node;
+    newValue->data = value;
+    newValue->nextValue = top;
+    return newValue;
+}
+
+Node* unstack(Node* top, char* value){
+    if(!top){
+        return NULL;
+    }
+    Node* temp = top;
+    *value = top->data;
+    top = top->nextValue;
+    delete temp;
+    return top;
+}
+
 int main(){
     return 0;
 }
