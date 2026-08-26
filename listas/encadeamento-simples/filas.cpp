a73ccb4f3774fbe63e7adf872cb8fd78d1b68695
abraaosantosdeveloper <abraaofilipi12@gmail.com>
2026-08-25 21:30:20 -0300

    Novo tipo de estrutura

---
+++ listas/encadeamento-simples/filas.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct Node {
+    char data[50];
+    Node* nextValue;
+} Node;
+
+typedef struct Queue {
+    Node* beginning;
+    Node* end;
+} Queue;
+
+int main(){
+
+    return 0;
+}
