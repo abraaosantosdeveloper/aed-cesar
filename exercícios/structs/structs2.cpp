c6cc21a80feab541e9106efac77f2bae78fcc00c
abraaosantosdev <abraaofilipi12@gmail.com>
2026-09-15 21:48:53 -0300

    Exercícios de listas

--- exercícios/structs/structs2.cpp
+++ exercícios/structs/structs2.cpp
@@ -2,30 +2,117 @@
 
 using namespace std;
 
-typedef struct Pessoa
+struct Student
 {
-    string nome;
-    int idade;
-    float altura;
-} Pessoa;
+    string name;
+    int registration;
+    float grade;
+};
+
+// Node for list creation
+struct Node
+{
+    Student student;
+    Node *next;
+};
+
+// Insert new node
+Node *createNode(Student student, Node *next)
+{
+    Node *newNode = new Node;
+    newNode->student = student;
+    newNode->next = next;
+
+    return newNode;
+}
+
+// Insert the created node in the end of the list
+void insertAsLast(Node *&head, Student student)
+{
+    Node *newNode = createNode(student, nullptr);
+
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+
+    Node *current = head;
+
+    while (current->next != nullptr)
+    {
+        current = current->next;
+    }
+
+    current->next = newNode;
+}
+
+// Acess the head node and return display the list information
+void displayList(Node *head)
+{
+    Node *current = head;
+
+    while (current != nullptr)
+    {
+        cout << "------------------" << endl;
+        cout << "Nome: " << current->student.name << endl;
+        cout << "Matricula: " << current->student.registration << endl;
+        cout << "Nota: " << current->student.grade << endl;
+        cout << "------------------" << endl;
+
+        current = current->next;
+    }
+}
+
+// Basic average grade
+float calcularMedia(float somatorioNotas, int dividendo)
+{
+    float media = somatorioNotas / dividendo;
+    return media;
+}
 
 int main()
 {
     /*========================================================*/
     //                                                        //
-    //                      Questão 1                         //
+    //                      Question 2                        //
     //                                                        //
     /*========================================================*/
 
-    Pessoa p;
+    // Create the head node
+    Node *head = NULL;
 
-    cout << "\nInsira o nome da pessoa: ";
-    cin >> p.nome;
-    cout << "Insira a idade da pessoa: ";
-    cin >> p.idade;
-    cout << "Insira a altura da pessoa: ";
-    cin >> p.altura;
-    cout << "\n\nNome: " << p.nome << "\nIdade: " << p.idade << "\nAltura: " << p.altura << "\n";
+    // Avg grade parameters
+    float somatorioNotas = 0;
+    int dividendo = 0;
+
+    for (int i = 0; i < 2; i++)
+    {
+        int counter = i + 1;
+
+        // Initialize an empty Student struct
+        Student student;
+
+        // Data entry
+        cout << "Digite o nome do aluno " << counter << ": ";
+        cin >> student.name;
+        cout << "Digite a matricula do aluno " << counter << ": ";
+        cin >> student.registration;
+        cout << "Digite a nota do aluno " << counter << ": ";
+        cin >> student.grade;
+
+        // Insert the last node containing the student struct
+        insertAsLast(head, student);
+
+        // Define the counter of the sum of all grades and quotient
+        somatorioNotas += student.grade;
+        dividendo += 1;
+    }
+
+    // Display the list
+    displayList(head);
+    // Display the avg grade
+    cout << "Media: " << calcularMedia(somatorioNotas, dividendo) << endl;
     system("Pause");
 
     return 0;
